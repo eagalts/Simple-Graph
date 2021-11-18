18bfa7545ae427f7272d1c49f50f8926ad952e99
Егор <33782691+eagalts@users.noreply.github.com>
2021-11-18 20:43:27 +0300

    Upload ver 1.0
    Graphics:
    - USD - RUB (20y)

---
+++ graph/graph/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace graph
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.USD = new System.Windows.Forms.Button();
+            this.info = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.IsSameFontSizeForAllAxes = true;
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Grayscale;
+            series2.BorderWidth = 3;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Color = System.Drawing.Color.Red;
+            series2.Font = new System.Drawing.Font("Cera Pro", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            series2.MarkerSize = 1;
+            series2.Name = "Курс доллара";
+            series2.ShadowColor = System.Drawing.Color.Black;
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(861, 426);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // USD
+            // 
+            this.USD.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.USD.Font = new System.Drawing.Font("Gilroy Bold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.USD.ForeColor = System.Drawing.Color.White;
+            this.USD.Location = new System.Drawing.Point(879, 12);
+            this.USD.Name = "USD";
+            this.USD.Size = new System.Drawing.Size(50, 50);
+            this.USD.TabIndex = 1;
+            this.USD.Text = "$";
+            this.USD.UseVisualStyleBackColor = true;
+            this.USD.Click += new System.EventHandler(this.USD_Click);
+            // 
+            // info
+            // 
+            this.info.Cursor = System.Windows.Forms.Cursors.Help;
+            this.info.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.info.Font = new System.Drawing.Font("Gilroy Bold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.info.ForeColor = System.Drawing.Color.White;
+            this.info.Location = new System.Drawing.Point(879, 388);
+            this.info.Name = "info";
+            this.info.Size = new System.Drawing.Size(50, 50);
+            this.info.TabIndex = 3;
+            this.info.Text = "?";
+            this.info.UseVisualStyleBackColor = true;
+            this.info.Click += new System.EventHandler(this.info_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(937, 450);
+            this.Controls.Add(this.info);
+            this.Controls.Add(this.USD);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button USD;
+        private System.Windows.Forms.Button info;
+    }
+}
+
---
+++ graph/graph/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace graph
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void USD_Click(object sender, EventArgs e)
+        {
+            int[] years = new int[21] { 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020 };
+            double[] data = new double[21] { 28.16, 29.17, 31.36, 30.67, 28.81, 28.30, 27.19, 25.58, 24.85, 31.85, 30.34, 29.39, 31.09, 31.82, 38.33, 61.15, 66.96, 58.25, 62.78, 64.55, 72.13 };
+
+            for (int i = 0; i < years.Length; i++)
+                chart1.Series["Курс доллара"].Points.AddXY(years[i], data[i]);
+
+            chart1.ChartAreas[0].AxisX.Minimum = 2000;
+            chart1.ChartAreas[0].AxisX.Maximum = 2020;
+
+            chart1.ChartAreas[0].AxisY.Minimum = 20;
+            chart1.ChartAreas[0].AxisY.Maximum = 80;
+
+        }
+
+        private void info_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Выберите кнопку со значением, график которого Вы хотите увидеть.\n\nSimpleGraphics\nver 1.0, 2021", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
